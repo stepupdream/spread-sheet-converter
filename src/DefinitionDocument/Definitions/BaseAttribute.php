2e68b2623bb7b795938dbc746d629d187c720d7b
stepupdream <stepupdream1002@gmail.com>
2022-07-18 15:48:12 +0900

    [refactor] corresponds to a two-dimensional array.

--- src/DefinitionDocument/Definitions/BaseAttribute.php
+++ src/DefinitionDocument/Definitions/BaseAttribute.php
@@ -1,22 +1,56 @@
 <?php
 
+declare(strict_types=1);
+
 namespace StepUpDream\SpreadSheetConverter\DefinitionDocument\Definitions;
 
-/**
- * Class BaseAttribute.
- */
+use LogicException;
+
 abstract class BaseAttribute
 {
     /**
      * Get attribute by key.
      *
+     * Get a string that indicates an array.
+     * It can be obtained as an array.
+     * Also, possible with a two-dimensional array.
+     *
      * @param  array  $attributes
      * @param  string  $headerKey
-     * @return mixed
+     * @return array
      */
-    protected function getAttributeByKey(array $attributes, string $headerKey)
+    protected function attributesByKey(array $attributes, string $headerKey): array
     {
-        return str_replace(PHP_EOL, '', $attributes[$headerKey]);
+        $attributesNotIndent = $this->attributeByKey($attributes, $headerKey);
+
+        $attributesNotIndent = rtrim(ltrim($attributesNotIndent));
+        if ($attributesNotIndent[0] !== '[' || ! str_ends_with($attributesNotIndent, ']')) {
+            throw new LogicException("It's not a string that matches the array: ".$attributesNotIndent);
+        }
+
+        $attributesNotIndent = substr($attributesNotIndent, 1);
+        $attributesNotIndent = substr($attributesNotIndent, 0, -1);
+        $attributesNotIndent = rtrim(ltrim($attributesNotIndent));
+        if ($attributesNotIndent[0] === '[' && str_ends_with($attributesNotIndent, ']')) {
+            // ex:attributeNotLineArray)
+            // [
+            //   0 => ''
+            //   1 => "1,2,3],"
+            //   2 => "1,2,3]"
+            // ]
+            $attributeNotLineArray = explode('[', $attributesNotIndent);
+            $elementCount = count($attributeNotLineArray);
+
+            $result = [];
+            for ($i = 1; $i < $elementCount; $i++) {
+                $beforeConvertText = strstr($attributeNotLineArray[$i], ']', true);
+                $result[] = explode(',', $beforeConvertText);
+            }
+
+            return $result;
+        }
+
+        return explode(',', $attributesNotIndent);
     }
 
     /**
@@ -24,17 +58,10 @@ abstract class BaseAttribute
      *
      * @param  array  $attributes
      * @param  string  $headerKey
-     * @return array
+     * @return string|array
      */
-    protected function getAttributeArrayByKey(array $attributes, string $headerKey): array
+    protected function attributeByKey(array $attributes, string $headerKey): string|array
     {
-        $attributeNotLine = $this->getAttributeByKey($attributes, $headerKey);
-
-        $attributeTrim = rtrim(ltrim($attributeNotLine));
-        if ($attributeTrim[0] === '[' && $attributeTrim[strlen($attributeTrim) - 1] === ']') {
-            return explode(',', str_replace(['[', ']'], '', $attributeTrim));
-        }
-
-        return explode(',', $attributeTrim);
+        return str_replace(PHP_EOL, '', $attributes[$headerKey]);
     }
 }
--- tests/DefinitionDocument/Definitions/AttributeTest.php
+++ tests/DefinitionDocument/Definitions/AttributeTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace StepUpDream\SpreadSheetConverter\Test\DefinitionDocument\Definitions;
 
 use StepUpDream\SpreadSheetConverter\DefinitionDocument\Definitions\Attribute;
@@ -19,13 +21,16 @@ class AttributeTest extends TestCase
         $attribute->setAttributeDetails('a', 'test1');
         $attribute->setAttributeDetails(PHP_EOL.'b'.PHP_EOL, 'test2');
         $attribute->setAttributeDetails('[1,2,3]', 'test3');
+        $attribute->setAttributeDetails('[[1,2,3],[1,2,3]]', 'test4');
 
         $response1 = $attribute->getAttributeDetailByKey('test1');
         $response2 = $attribute->getAttributeDetailByKey('test2');
-        $response3 = $attribute->getAttributeDetailArrayByKey('test3');
+        $response3 = $attribute->getAttributeDetailsByKey('test3');
+        $response4 = $attribute->getAttributeDetailsByKey('test4');
 
         self::assertEquals('a', $response1);
         self::assertEquals('b', $response2);
         self::assertEquals([1, 2, 3], $response3);
+        self::assertEquals([[1, 2, 3], [1, 2, 3]], $response4);
     }
 }
